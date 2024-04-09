a23fc9f266d2d38159cf9f7a299866f403713a2e
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-04-09 22:49:55 +0300

    create script file and fetch github user

---
+++ utilities/fetchGithubUser.js
@@ -0,0 +1,23 @@
+const fetchGithubUser = () => {
+  const searchButton = document.querySelector('.search-btn');
+  const github = new Github();
+  const ui = new UI();
+
+  github.getUser('octocat').then((data) => ui.showProfile(data.profile));
+
+  searchButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (ui.searchInput.value === '') return;
+
+    github.getUser(ui.searchInput.value).then((data) => {
+      data.profile.message === 'Not Found'
+        ? ui.showAlert('No results', 'not-found')
+        : ui.showProfile(data.profile);
+    });
+
+    ui.clearProfile();
+    ui.searchInput.value = '';
+  });
+};
+
+export default fetchGithubUser;
