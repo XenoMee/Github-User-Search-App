e6983d62205b78bc13a4a6e7342fdd8094793fc7
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-04-09 14:05:31 +0300

    create script file for fetching github user data

---
+++ utilities/github.js
@@ -0,0 +1,14 @@
+class Github {
+  constructor() {
+    this.client_id = 'd09427061eb4e8a1022f';
+  }
+
+  async getUser(user) {
+    const profileRes = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}`
+    );
+    const profileData = await profileRes.json();
+
+    return { profileData };
+  }
+}
