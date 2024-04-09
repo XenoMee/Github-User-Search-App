b3f814a659a2805b92dcc5afaba69058fb27dc5c
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-04-09 12:30:19 +0300

    import toggle dark mode function inside main script

---
+++ main.js
@@ -0,0 +1,6 @@
+import toggleDarkMode from './utilities/toggleDarkMode.js';
+
+const userInput = document.querySelector('#search-input');
+const searchButton = document.querySelector('.search-btn');
+
+toggleDarkMode();
